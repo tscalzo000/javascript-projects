7911b0368b07c081706c4f340d17cb50ef8bf348
vscalzo000 <vscalzo000@gmail.com>
2017-04-09 19:13:27 -0400

    Stacks: Balanced Brackets

---
+++ challenges/stacks_balanced_brackets/solution.js
@@ -0,0 +1,49 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+  input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+  input_stdin_array = input_stdin.split("\n");
+  main();
+});
+
+function readLine() {
+  return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+  var t = parseInt(readLine());
+  for(var a0 = 0; a0 < t; a0++){
+    var expression = readLine();
+    var parens = expression.replace(/[^\[\]{}()]/g, "");
+    parens = parens.split("");
+    var opens = [];
+    var close = [];
+    for (var i = 0; i < parens.length; i++) {
+      if (["[","{","("].includes(parens[i])) {
+        opens.push(parens[i]);
+      } else {
+        var length = opens.length;
+        if (["[]","{}","()"].includes( opens[length-1] + parens[i] )) {
+          opens.pop();
+        } else {
+          close.push(parens[i]);
+        }
+      }
+    }
+    if ((opens.length > 0) || (close.length > 0)) {
+      console.log('NO');
+    } else {
+      console.log('YES');
+    }
+  }
+}
